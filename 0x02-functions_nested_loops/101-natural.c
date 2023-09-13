1781d6387da62fca25ae78c3764e12c940f57043
Chisom2023 <michaelchisom04@gmail.com>
2023-09-12 19:13:53 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,28 +1,48 @@
 #include <stdio.h>
 
+/**
+  * main - Fibonacci sequence up to a certain limit
+  *
+  * This prog cal and prints the Fib seq up to a certain limit.
+  *
+  * Return: 0 (Success)
+  */
 
-	/**
-	  * -natural.c - Lists all the natural numbers below 1024 (excluded)
-	  * that are multiples of 3 or 5.
-	  *
-	  * Return: Always 0.
-	  */
+int main(void)
+{
 
-	int main(void)
-	{
+unsigned long int i;
+unsigned long int bef = 1;
+unsigned long int aft = 2;
+unsigned long int l = 1000000000;
+unsigned long int bef1;
+unsigned long int bef2;
+unsigned long int aft1;
+unsigned long int aft2;
 
-		int i, sum = 0;
+printf("%lu", bef);
+for (i = 1; i < 91; i++)
+{
+printf(", %lu", aft);
+aft += bef;
+bef = aft - bef;
+}
 
-		for (i = 0; i < 1024; i++)
-		{
-			if ((i % 3) == 0 || (i % 5) == 0)
-				sum += i;
-		}
+bef1 = (bef / l);
+bef2 = (bef % l);
+aft1 = (aft / l);
+aft2 = (aft % l);
 
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", aft1 + (aft2 / l));
+printf("%lu", aft2 % l);
+aft1 = aft1 + bef1;
+bef1 = aft1 - bef1;
+aft2 = aft2 + bef2;
+bef2 = aft2 - bef2;
+}
+printf("\n");
+return (0);
 
-		printf("%d\n", sum);
-
-
-		return (0);
-
-	}
+}
