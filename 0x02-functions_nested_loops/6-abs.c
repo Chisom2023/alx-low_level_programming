ac9980be32f4e9f99347b8837a4113cad489ef7f
Chisom2023 <michaelchisom04@gmail.com>
2023-09-12 17:09:08 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
-/**
-  *_abs - Computes the absolute
-  *@c: The number computed
-  *Return: Absolute value
-  */
 
+/**
+ * _abs - Computers the absolute
+ * @c: The number computed
+ * Return: Absolute value
+ */
 int _abs(int c)
 {
-
-	if (c > 0)
+	if (c < 0)
 	{
 		int abs_val;
+
 
 		abs_val = c * -1;
 		return (abs_val);
