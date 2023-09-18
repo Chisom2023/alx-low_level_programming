8e5ae88ad18cab9c116739774947d55a0dec30bd
Chisom2023 <michaelchisom04@gmail.com>
2023-09-18 14:06:04 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _rev - the function name
+  * @s : parameter of the function
+  */
+
+void print_rev(char *s)
+{
+int i;
+int count = 0;
+
+for (i = 0; s[i] != '\0'; i++)
+	count++;
+
+for (i = count - 1; i >= 0; i--)
+{
+	_putchar (s[i]);
+}
+	_putchar ('\n');
+}
