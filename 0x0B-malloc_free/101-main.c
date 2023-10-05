bc56c4ee2a956991d6cfe7bfda80e15b43eac5ec
Chisom2023 <michaelchisom04@gmail.com>
2023-10-04 19:40:31 -0700

    101-strtow.c

--- 0x0B-malloc_free/101-main.c
+++ 0x0B-malloc_free/101-main.c
@@ -1,9 +1,10 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * print_tab - Prints an array of string
+ * print_tab - Prints an array of strings
  * @tab: The array to print
  *
  * Return: nothing
@@ -18,21 +19,55 @@ void print_tab(char **tab)
     }
 }
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: 1 if an error occurred, 0 otherwise
- */
 int main(void)
 {
+    char input[] = "      ALX School         #cisfun      ";
     char **tab;
+    char *token;
+    int i = 0;
+    int j;
 
-    tab = strtow("      ALX School         #cisfun      ");
+    /* Allocate memory for the tab array */
+    tab = (char **)malloc(sizeof(char *));
     if (tab == NULL)
     {
-        printf("Failed\n");
+        printf("Failed to allocate memory\n");
         return (1);
     }
+
+    /* Tokenize the input string and store the words in the tab array */
+    token = strtok(input, " ");
+    while (token != NULL)
+    {
+        tab[i] = strdup(token); /* Duplicate the token */
+        if (tab[i] == NULL)
+        {
+            printf("Failed to allocate memory\n");
+            return (1);
+        }
+        i++;
+
+        /* Resize the tab array */
+        tab = (char **)realloc(tab, (i + 1) * sizeof(char *));
+        if (tab == NULL)
+        {
+            printf("Failed to allocate memory\n");
+            return (1);
+        }
+
+        token = strtok(NULL, " ");
+    }
+
+    tab[i] = NULL; /* Null-terminate the tab array */
+
     print_tab(tab);
+
+    /* Free the allocated memory */
+    for (j = 0; j < i; j++)
+    {
+        free(tab[j]);
+    }
+    free(tab);
+
     return (0);
 }
